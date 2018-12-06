a4bc0852a58578d284d1f83c9aaebf540727490d
Rafael Rabetti <rafael.rabetti@gmail.com>
2018-12-06 01:39:43 -0200

    Começando requisição http no haskell.

--- api/app/Main.hs
+++ api/app/Main.hs
@@ -1,11 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
 import Web.Scotty as S
 
--- main :: IO ()
--- main = do
---     scotty 3000 $ do
---         middleware simpleCors
+type Description = String
+type Status = String
 
---         get "/"
+allIssues :: M.Map Status [Description]
+allIssues = M.fromList
+  [ ( "done", 
+      [ "Dividr em 5 boards"
+      , "Criar arquivdar e desarquivar"
+      ]
+    )
+  , ( "doing",
+      [ "Importar de JSON"
+      , "Integrar com o Haskell"  
+      ]
+    )
+  , ( "todo",
+      [ "Exportar para JSON"
+      , "Deixar mais bonito"
+      ]
+    )
+  , ( "backlog",
+      [ "Criar label para as issues"
+      , "Criar comentáro na issue"
+      ]
+    ) 
+  ]
+
+main :: IO ()
+main = do
+    issues' <- newMVar  allIssues
+
+    scotty 3000 $ do
+        middleware simpleCors
+
+        get "/issues" $ do
+          issues <- liftIO $ readMVar issues'
+          json issues
